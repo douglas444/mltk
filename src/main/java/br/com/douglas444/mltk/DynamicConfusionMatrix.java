ff9bef6ffba524154966df367532a8c9c289e203
Douglas <douglas.m.cavalcanti@gmail.com>
2019-07-04 10:32:47 -0300

    minor corrections

--- src/main/java/br/com/douglas444/mltk/DynamicConfusionMatrix.java
+++ src/main/java/br/com/douglas444/mltk/DynamicConfusionMatrix.java
@@ -1,9 +1,6 @@
 package br.com.douglas444.mltk;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class DynamicConfusionMatrix {
 
@@ -20,6 +17,9 @@ public class DynamicConfusionMatrix {
 
     public DynamicConfusionMatrix(List<Integer> knownLabels) {
 
+        this.actualLabelsCount = 0;
+        this.predictedLabelsCount = 0;
+
         actualLabelEnumByActualLabel = new HashMap<>();
         predictedLabelEnumByPredictedLabel = new HashMap<>();
 
@@ -32,15 +32,17 @@ public class DynamicConfusionMatrix {
         knownLabels.forEach(label -> {
 
             actualLabelEnumByActualLabel.put(label, actualLabelsCount);
-            predictedLabelEnumByPredictedLabel.put(label, actualLabelsCount);
+            predictedLabelEnumByPredictedLabel.put(label, predictedLabelsCount);
             ++actualLabelsCount;
+            ++predictedLabelsCount;
 
-            this.predictionsAsKnownByActualLabel.put(label,
+            this.predictionsAsKnownByActualLabel.put(actualLabelEnumByActualLabel.get(label),
                     new ArrayList<>(Collections.nCopies(knownLabels.size(), 0)));
 
-            this.predictionsAsNovelByActualLabel.put(label, new ArrayList<>());
+            this.predictionsAsNovelByActualLabel.put(actualLabelEnumByActualLabel.get(label), new ArrayList<>());
 
         });
+
 
     }
 
@@ -49,17 +51,18 @@ public class DynamicConfusionMatrix {
 
         if (actualLabelEnumByActualLabel.get(actualLabel) == null) {
 
-            actualLabelEnumByActualLabel.put(actualLabelsCount, actualLabel);
-            predictionsAsKnownByActualLabel.put(actualLabelsCount, Collections.nCopies(knownPredictedLabelsCount, 0));
-            predictionsAsNovelByActualLabel.put(actualLabelsCount, Collections.nCopies(novelPredictedLabelsCount, 0));
+            actualLabelEnumByActualLabel.put(actualLabel, actualLabelsCount);
+            predictionsAsKnownByActualLabel.put(actualLabelsCount, new ArrayList<>(Collections.nCopies(knownPredictedLabelsCount, 0)));
+            predictionsAsNovelByActualLabel.put(actualLabelsCount, new ArrayList<>(Collections.nCopies(novelPredictedLabelsCount, 0)));
             ++actualLabelsCount;
         }
 
         if (predictedLabelEnumByPredictedLabel.get(predictedLabel) == null) {
 
-            predictedLabelEnumByPredictedLabel.put(predictedLabelsCount, predictedLabel);
+            predictedLabelEnumByPredictedLabel.put(predictedLabel, predictedLabelsCount);
             predictionsAsNovelByActualLabel.forEach((key, value) -> value.add(0));
             ++predictedLabelsCount;
+            ++novelPredictedLabelsCount;
         }
 
         int actualLabelEnum = actualLabelEnumByActualLabel.get(actualLabel);
@@ -68,16 +71,53 @@ public class DynamicConfusionMatrix {
         if (isNovel) {
 
             int count = predictionsAsNovelByActualLabel.get(actualLabelEnum).get(predictedLabelEnum);
-            predictionsAsNovelByActualLabel.get(actualLabelEnum).add(predictedLabelEnum, count + 1);
+            predictionsAsNovelByActualLabel.get(actualLabelEnum).set(predictedLabelEnum, count + 1);
 
         } else {
 
+
             int count = predictionsAsKnownByActualLabel.get(actualLabelEnum).get(predictedLabelEnum);
-            predictionsAsKnownByActualLabel.get(actualLabelEnum).add(predictedLabelEnum, count + 1);
+            predictionsAsKnownByActualLabel.get(actualLabelEnum).set(predictedLabelEnum, count + 1);
 
         }
 
     }
 
+    @Override
+    public String toString() {
+        int[][] matrix = new int[actualLabelsCount][predictedLabelsCount];
 
+        int count = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : predictionsAsKnownByActualLabel.entrySet()) {
+
+            List<Integer> value = entry.getValue();
+
+            for (int i = 0; i < value.size(); ++i) {
+                matrix[count][i] = value.get(i);
+            }
+            ++count;
+        }
+
+        count = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : predictionsAsNovelByActualLabel.entrySet()) {
+
+            List<Integer> value = entry.getValue();
+
+            for (int i = 0; i < value.size(); ++i) {
+                matrix[count][knownPredictedLabelsCount + i] = value.get(i);
+            }
+            ++count;
+
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                stringBuilder.append(String.format("|%6d", anInt));
+            }
+            stringBuilder.append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
 }
